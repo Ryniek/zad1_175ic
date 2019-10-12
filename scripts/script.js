327abd2e15b09b77ba114f5ee232701d42d9945d
Michal Rynski <michalrynski96@gmail.com>
2019-10-13 00:16:55 +0200

    Final release

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
-/*function onClickHandler() {
-    var text = document.getElementById('js-test');
-    console.log(text);
-}*/
+function onClickHandler() {
+        var figure = document.getElementById('comments-figure');
+        var text = document.getElementById('content-field').value;
+        var text2 = document.getElementById('author-field').value;
+        var comment = document.createElement('figure')
+        var content = document.createElement('p');
+        var author = document.createElement('label');
+        content.classList.add('comment-area');
+        content.style.fontWeight = '600';
+        content.innerHTML = text;
+        author.classList.add('author-field');
+        author.style.fontWeight = '600';
+        author.innerHTML = 'Author: ' + text2;
+        figure.appendChild(comment);
+        comment.appendChild(content);
+        comment.appendChild(author);
+}
